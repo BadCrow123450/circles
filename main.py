0e099536eb0522c42d32a1a190510ef1c391f34f
xorifuma <anprokudin19508@gmail.com>
2024-12-11 17:30:26 +0500

    Добавлена возможность получать круг случайного цвета

--- main.py
+++ main.py
@@ -4,48 +4,30 @@ import sys
 import random
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtCore import QPointF
+from PyQt6 import QtCore, QtGui, QtWidgets
 
 
-template = """<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>Form</class>
- <widget class="QWidget" name="Form">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>507</width>
-    <height>435</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>Form</string>
-  </property>
-  <widget class="QPushButton" name="pushButton">
-   <property name="geometry">
-    <rect>
-     <x>370</x>
-     <y>400</y>
-     <width>131</width>
-     <height>31</height>
-    </rect>
-   </property>
-   <property name="text">
-    <string>push to create circle</string>
-   </property>
-  </widget>
- </widget>
- <resources/>
- <connections/>
-</ui>
-"""
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(507, 435)
+        self.pushButton = QtWidgets.QPushButton(parent=Form)
+        self.pushButton.setGeometry(QtCore.QRect(370, 400, 131, 31))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "push to create circle"))
 
 
-class MainW(QtWidgets.QMainWindow):
+class MainW(QtWidgets.QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
-        f = io.StringIO(template)
-        uic.loadUi(f, self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.random_circle)
         self.need_draw = False
 
@@ -56,7 +38,7 @@ class MainW(QtWidgets.QMainWindow):
         if self.need_draw:
             painter = QPainter()
             painter.begin(self)
-            painter.setBrush(QColor(255, 255, 0))
+            painter.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
             radius = random.randint(20, 200)
             painter.drawEllipse(QPointF(250, 250), radius, radius)
             painter.end()
